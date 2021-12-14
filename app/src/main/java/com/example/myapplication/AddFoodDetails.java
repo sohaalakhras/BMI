621af660098a77defec61dc88ab9bef85bd1843e
PC <sohaalakhras57@gmail.com>
2021-12-14 21:22:33 +0200

    add food details

---
+++ app/src/main/java/com/example/myapplication/AddFoodDetails.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddFoodDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_food_details);
+    }
+}
